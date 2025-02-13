780183784344a6c2e818934954c8677fef57bb08
ctjutting <100168643+ctjutting@users.noreply.github.com>
2025-02-12 20:30:00 -0600

    xpath

--- screenshot.py
+++ screenshot.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
 # Set up Chrome options for headless mode
 chrome_options = Options()
@@ -23,16 +24,14 @@ driver.set_window_size(2560, 1440)
 # Open the webpage
 driver.get("https://meshmap.iowamesh.net/?lat=41.9953615365105&lng=267.2328359397183&zoom=9")
 
-# Wait for the element to be present and get its location
+# Wait for the element to be present and click it using XPath
 try:
     close_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.CSS_SELECTOR, ".h-7:nth-child(1) path:nth-child(3)"))
+        EC.element_to_be_clickable((By.XPATH, "//*[@class='h-7'][1]/*[name()='path'][3]"))
     )
-    location = close_button.location
-    print("Close button location:", location)
-    
-    # Use JavaScript to simulate a click event at the specified coordinates
-    driver.execute_script(f"document.elementFromPoint({location['x']}, {location['y']}).click();")
+    # Use ActionChains to move to the element and click it
+    actions = ActionChains(driver)
+    actions.move_to_element(close_button).click().perform()
 except Exception as e:
     print("Close button not found or could not be clicked:", e)
 
@@ -41,4 +40,12 @@ screenshots_dir = "screenshots"
 if not os.path.exists(screenshots_dir):
     os.makedirs(screenshots_dir)
 
-# Generate the
+# Generate the filename with date and time stamp
+timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+filename = os.path.join(screenshots_dir, f"screenshot_{timestamp}.png")
+
+# Take a screenshot and save it to the output file
+driver.save_screenshot(filename)
+
+# Close the browser
+driver.quit()
