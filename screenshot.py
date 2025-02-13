18b3dda7daf35bbd27da41221556dd133579c57e
ctjutting <100168643+ctjutting@users.noreply.github.com>
2025-02-12 18:43:45 -0600

    up

--- screenshot.py
+++ screenshot.py
@@ -1,6 +1,8 @@
+import selenium
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 from datetime import datetime
 import os
 
